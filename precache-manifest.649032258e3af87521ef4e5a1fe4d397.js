25cd00c7438eb930a94fc093c6f1e4e57d22ab41
Naman Agarwal <thenamanagarwal@gmail.com>
2020-08-13 02:31:44 +0530

    Updates

---
+++ precache-manifest.649032258e3af87521ef4e5a1fe4d397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb28fde422a4496dd52c275fa245d10c",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "2e68ca17a6ba6f06ff58",
+    "url": "/smartbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f26c584ac4610ab0d4f6",
+    "url": "/smartbrain/static/css/main.7bd8497c.chunk.css"
+  },
+  {
+    "revision": "2e68ca17a6ba6f06ff58",
+    "url": "/smartbrain/static/js/2.6f342396.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smartbrain/static/js/2.6f342396.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f26c584ac4610ab0d4f6",
+    "url": "/smartbrain/static/js/main.5afc2242.chunk.js"
+  },
+  {
+    "revision": "ae456e4515232efad5b0",
+    "url": "/smartbrain/static/js/runtime-main.b715ec47.js"
+  }
+]);
